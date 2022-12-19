2a340e4015c13b932bfb68a435624ddbf4576a13
Poojabodake2001 <poojabodake5284@gmail.com>
2022-12-19 20:05:04 +0530

    country flags

--- assets/js/js app.js
+++ assets/js/js app.js
@@ -1,35 +1,28 @@
 var cl = console.log;
-cl(movieArr)
+cl(countries)
 
-var movieContainer = document.getElementById('movieContainer');
+var countryContainer = document.getElementById('countryContainer');
 var result = '';
-movieArr.forEach(function (movie) {
+countries.forEach(function (country) {
   result += `
     <div class = "col-lg-3">
       <div class= "card">
-        <figure class = "movieCard">
-          <img src = "https://image.tmdb.org/t/p/w1280/${movie.poster_path}"alt="${movie.title}">
-          <figcaption class = "text-white p-4 bg-dark">
-            <div class = "row">
-              <div class = "col-sm-10">
-                <h3>
-                  ${movie.title}
-                </h3>
-              </div>
-              <div class = "col-sm-2">
-                ${movie.vote_average}
-              </div>
-            </div>
-          </figcaption> 
-            <div class = "overview bg -white p-4">
-              <h1>overview</h1>
-              <p>${movie.overview} </p> 
-            </div>
-        </figure> 
-      </div>
-    </div>     
-
-    `
+        <figure class = "countryCard">
+          <img src = "${country.flag}">
+          <figcaption>
+          <div class="cName">
+              <h2>${country.name}</h2>
+          </div>
+          <div class="country-Data">
+              <ul>
+                  <li><strong>Capital: </strong>${country.capital}</li>
+                  <li><strong>Language: </strong>${country.languages} </li>
+                  <li><strong>Population: </strong>${country.population}</li>
+              </ul>
+          </div>
+      </figcaption>
+  </figure>
+</div>
+</div>`;
 })
-
-movieContainer.innerHTML = result;
+countryContainer.innerHTML = result;
